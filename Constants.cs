91145c59ba8863b3666330b0c263d5df692ef51c
Igor Lysenko <igor18052003@gmail.com>
2025-04-29 19:54:15 +0300

    feat: Project setup

---
+++ Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace AvitoSniffer
+{
+    internal class Constants
+    {
+        public const string gotoUrl = "https://www.avito.ru/sevastopol/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?context=H4sIAAAAAAAA_wEkANv_YToxOntzOjg6ImZyb21QYWdlIjtzOjg6InZlcnRpY2FsIjt938025iQAAAA&f=ASgBAQICBESSA8gQ8AeQUsDBDbr9N~bwDvKl4wIBQMwIJJBZjlk";
+    }
+}
